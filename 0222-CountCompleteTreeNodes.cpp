ab8a371676ff03061881b33dbacec723c4057f16
Amit Singh Rana <ranaa73000@gmail.com>
2022-04-01 17:26:19 +0530

    'Count Complete Tree Nodes'  --MEDIUM--

---
+++ 0222-CountCompleteTreeNodes.cpp
@@ -0,0 +1,33 @@
+// Time Complexity - O(logN)
+// Space Complexity - O(N), for call stack
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root)   return 0;
+        
+        int lh = 0, rh = 0;
+        
+        TreeNode* tmp = root;
+        while(tmp->left) {          // Calculate left-subtree height
+            ++lh;
+            tmp = tmp->left;
+        }
+        
+        tmp = root;
+        while(tmp->right) {         // Calculate right-subtree height
+            ++rh;
+            tmp = tmp->right;
+        }
+        
+        if(lh == rh) {                  // Complete Binary Tree, since left-height = right-height
+            return (pow(2, lh+1)-1);    // No. of elements of complete BT
+        }
+        
+        // Not a complete BT
+        int left  = countNodes(root->left);
+        int right = countNodes(root->right);
+        
+        return (1 + left + right);          // No of elements = cnt(elements on left) + cnt(elements on right) + 1(root element)
+    }
+};
